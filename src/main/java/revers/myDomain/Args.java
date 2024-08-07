4b534b73e2e77aa51f9d8404c57af2bcd9405846
KOT-BaJlEPA <regulusblack1991@gmail.com>
2024-08-07 15:29:15 +0300

    create Args.updateArguments()

--- src/main/java/revers/myDomain/Args.java
+++ src/main/java/revers/myDomain/Args.java
@@ -1,6 +1,37 @@
 package revers.myDomain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Args {
+
+    private static Map<String,Boolean> inputArgument = new HashMap<String,Boolean>();
+
+    public static void updateArguments(String ...args){
+        inputArgument.putIfAbsent("dirForResult", false);
+        inputArgument.putIfAbsent("prefixToResultFile", false);
+        inputArgument.putIfAbsent("addToResultFile", false);
+        inputArgument.putIfAbsent("shortStatistic", false);
+        inputArgument.putIfAbsent("longStatistic", false);
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-o")) {
+                inputArgument.put("dirForResult", true);
+            }else if (args[i].equals("-p")) {
+                inputArgument.put("prefixToResultFile", true);
+            }else if (args[i].equals("-a")) {
+                inputArgument.put("addToResultFile", true);
+            }else if (args[i].equals("-s")) {
+                inputArgument.put("shortStatistic", true);
+            }else if (args[i].equals("-f")) {
+                inputArgument.put("longStatistic", true);
+            }
+        }
+    }
+
+
+
+
     public static boolean checkArgs(String[] args) {
         if (args.length < 1) {
             return false;
