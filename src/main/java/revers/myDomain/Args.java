f5eebec92e646067350b608f0e1f99f7fd0d9a2d
KOT-BaJlEPA <regulusblack1991@gmail.com>
2024-08-07 18:23:49 +0300

    create Args.updateArguments()

--- src/main/java/revers/myDomain/Args.java
+++ src/main/java/revers/myDomain/Args.java
@@ -2,6 +2,8 @@ package revers.myDomain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Args {
 
@@ -15,16 +17,20 @@ public class Args {
         inputArgument.putIfAbsent("longStatistic", false);
 
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-o")) {
+            //шаблон для проверки что аргумент похож на имя файла
+            Pattern patternNameFileTxt = Pattern.compile(".*.txt");
+            if (args[i].trim().equals("-o")) {
                 inputArgument.put("dirForResult", true);
-            }else if (args[i].equals("-p")) {
+            }else if (args[i].trim().equals("-p")) {
                 inputArgument.put("prefixToResultFile", true);
-            }else if (args[i].equals("-a")) {
+            }else if (args[i].trim().equals("-a")) {
                 inputArgument.put("addToResultFile", true);
-            }else if (args[i].equals("-s")) {
+            }else if (args[i].trim().equals("-s")) {
                 inputArgument.put("shortStatistic", true);
-            }else if (args[i].equals("-f")) {
+            }else if (args[i].trim().equals("-f")) {
                 inputArgument.put("longStatistic", true);
+            } else if (patternNameFileTxt.matcher(args[i]).matches()) {
+                String fileName = args[i];
             }
         }
     }
--- src/main/java/revers/myDomain/Main.java
+++ src/main/java/revers/myDomain/Main.java
@@ -2,11 +2,22 @@ package revers.myDomain;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
 
         String currentDir = new File("").getAbsolutePath();
+
+        String ssr = "yl89.fk.txt";
+
+        Pattern pattern = Pattern.compile(".*.txt$");
+        System.out.println(pattern.matcher(ssr).matches());
+
+
+
+
 //        integers.txt, floats.txt, strings.txt
 //        Дополнительно с помощью опции -o нужно уметь задавать путь для результатов.
 //        Опция -p задает префикс имен выходных файлов.
